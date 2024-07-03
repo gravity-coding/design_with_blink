8b329355698eb8026d875dad1fc4307d97e6ab4e
Amarjeet Chaudhary <thesiyhbrand@gmail.com>
2024-07-03 22:54:11 +0530

    Updated part-4

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ document.querySelector("body").addEventListener("mousemove", function (details) 
     document.querySelector(".cursor-move").style.left = `${details.clientX}px`
 });
 
+document.querySelector(".part-3").addEventListener("mouseenter", function () {
+    document.querySelector(".cursor-move").style.display = "flex"
+})
+
+document.querySelector(".part-3").addEventListener("mouseleave", function () {
+    document.querySelector(".cursor-move").style.display = "none"
+})
+
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
